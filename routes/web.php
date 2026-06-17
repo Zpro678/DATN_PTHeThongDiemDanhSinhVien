a48f762f47667ef19c7d14e31454e7ce8f3b9f5a
Zpro678 <minhhieut947@gmail.com>
2026-06-17 12:10:18 +0700

    do feature manual attendance

--- routes/web.php
+++ routes/web.php
@@ -86,6 +86,11 @@ Route::middleware(['auth', 'verified', 'role:teacher'])
     ->group(function () {
         Route::redirect('/courses', '/lecturer/classes')->name('lecturer.courses');
         Route::get('/attendance', [AttendanceSessionController::class, 'index'])->name('lecturer.attendance');
+        
+        Route::get('/manual',[AttendanceSessionController::class,'manualAttendance'])->name('lecturer.attendance.manual');
+        
+        Route::match(['get', 'post'], '/manual-active',[AttendanceSessionController::class,'activeManualAttendance'])->name('lecturer.attendance.manual.manual-active');
+
         Route::view('/analytics', 'lecture.analytics.main')->name('lecturer.analytics');
 
         Route::resource('classes', ClassController::class);
@@ -177,6 +182,5 @@ Route::middleware(['auth'])->prefix('student')->name('student.')->group(function
         return view('student.profile.studentProfile');
     })->name('profile');                              // /student/profile
 });
-
 
 
