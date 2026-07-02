fb2c2f1ed64bf7a0baf53f5dac80b21ac5974053
NguyenQuangLeKhoi <0306231120@caothang.edu.vn>
2026-07-02 19:30:48 +0700

    brevo

--- test_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Mail;
-
-Mail::raw('Thử nghiệm gửi mail bằng Mailjet qua cổng 2525 thành công!', function ($message) {
-    $message->to('cdth23webbnhom3@gmail.com')
-            ->subject('Test kết nối Mailjet SMTP');
-});
-
-echo "Gửi thành công!\n";
